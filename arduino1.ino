3de3f35fc6c7f3cbb4ecad64ce4101613b1be606
Yago Lopez <90837398+yagolscpt@users.noreply.github.com>
2022-05-19 12:03:27 +0200

    Add files via upload

---
+++ arduino1.ino
@@ -0,0 +1,15 @@
+// C++ code
+//
+int led=13;
+void setup()
+{
+  pinMode(led, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(led, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
